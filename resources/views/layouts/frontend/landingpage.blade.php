549dc53147b9dbbf371e54bc221592543a229340
lazuardi <lazuardi.reznnov@gmail.com>
2022-12-15 20:22:59 +0700

    landingpage
    about , Portofolio & contac Section

--- resources/views/layouts/frontend/landingpage.blade.php
+++ resources/views/layouts/frontend/landingpage.blade.php
@@ -9,7 +9,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-            href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700&family=Poppins:ital,wght@0,100;0,200;0,300;0,500;0,700;1,600&family=Roboto:wght@300;400;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;4 mb-200;700&family=Poppins:ital,wght@0,100;0,200;0,300;0,500;0,700;1,600&family=Roboto:wght@300;400;700&display=swap"
             rel="stylesheet"
         />
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
@@ -107,20 +107,215 @@
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-md-6 text-center">
-                        <h2 class="text-light text-shadow">
+                        <h2 class="text-light text-shadow-lg">
                             Lazuardi <i class="bi bi-activity"></i> Code
                         </h2>
-                        <p class="text-muted">Web Design | Program</p>
-                        <p class="text-blue-700">
+                        <p class="text-blue-100">Web Design | Program</p>
+                        <p class="text-blue-100">
                             Lorem ipsum dolor sit amet consectetur adipisicing
                             elit. Doloribus dolorum ipsum mollitia earum magni
                             minus quidem. Voluptatum in blanditiis odio.
                         </p>
+                        <button class="btn btn-primary">Enter</button>
                     </div>
                 </div>
             </div>
         </div>
-
         <!-- Endjumbotron -->
+
+        <!-- Content -->
+        <main>
+            <div class="container">
+                <div class="mb-5">
+                    <h1 class="text-center mb-3 text-blue-800 text-shadow">
+                        About
+                    </h1>
+                    <div class="row justify-content-center">
+                        <div class="col-md-5">
+                            <p>
+                                Lorem ipsum, dolor sit amet consectetur
+                                adipisicing elit. Eius maxime recusandae vel
+                                quas commodi illo porro perferendis expedita
+                                ratione suscipit.
+                            </p>
+                        </div>
+                        <div class="col-md-5">
+                            <p>
+                                Lorem ipsum, dolor sit amet consectetur
+                                adipisicing elit. Eius maxime recusandae vel
+                                quas commodi illo porro perferendis expedita
+                                ratione suscipit.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="bg-blue-900 rounded p-3">
+                    <h1 class="text-blue-100 text-shadow text-center mb-4">
+                        Portofolio
+                    </h1>
+                    <div class="row">
+                        <div class="col-md-4 mb-2">
+                            <div class="card">
+                                <img
+                                    src="http://source.unsplash.com/200x200?truck"
+                                    alt=""
+                                />
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">
+                                        Some quick example text to build on the
+                                        card title and make up the bulk of the
+                                        card's content.
+                                    </p>
+                                    <a href="#" class="btn btn-primary"
+                                        >Go somewhere</a
+                                    >
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 mb-2">
+                            <div class="card">
+                                <img
+                                    src="http://source.unsplash.com/200x200?car"
+                                    alt=""
+                                />
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">
+                                        Some quick example text to build on the
+                                        card title and make up the bulk of the
+                                        card's content.
+                                    </p>
+                                    <a href="#" class="btn btn-primary"
+                                        >Go somewhere</a
+                                    >
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 mb-2">
+                            <div class="card">
+                                <img
+                                    src="http://source.unsplash.com/200x200?cartoon"
+                                    alt=""
+                                />
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">
+                                        Some quick example text to build on the
+                                        card title and make up the bulk of the
+                                        card's content.
+                                    </p>
+                                    <a href="#" class="btn btn-primary"
+                                        >Go somewhere</a
+                                    >
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 mb-2">
+                            <div class="card">
+                                <img
+                                    src="http://source.unsplash.com/200x200?transformer"
+                                    alt=""
+                                />
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">
+                                        Some quick example text to build on the
+                                        card title and make up the bulk of the
+                                        card's content.
+                                    </p>
+                                    <a href="#" class="btn btn-primary"
+                                        >Go somewhere</a
+                                    >
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 mb-2">
+                            <div class="card">
+                                <img
+                                    src="http://source.unsplash.com/200x200?ford"
+                                    alt=""
+                                />
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">
+                                        Some quick example text to build on the
+                                        card title and make up the bulk of the
+                                        card's content.
+                                    </p>
+                                    <a href="#" class="btn btn-primary"
+                                        >Go somewhere</a
+                                    >
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 mb-2">
+                            <div class="card">
+                                <img
+                                    src="http://source.unsplash.com/200x200?truck"
+                                    alt=""
+                                />
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">
+                                        Some quick example text to build on the
+                                        card title and make up the bulk of the
+                                        card's content.
+                                    </p>
+                                    <a href="#" class="btn btn-primary"
+                                        >Go somewhere</a
+                                    >
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="p-3">
+                    <h1 class="text-center">Contac Us</h1>
+                    <div class="row justify-content-center">
+                        <div class="col-md-6">
+                            <div class="card">
+                                <div class="card-header">Form Contac</div>
+                                <div class="card-body">
+                                    <div class="mb-3">
+                                        <label
+                                            for="exampleFormControlInput1"
+                                            class="form-label"
+                                            >Email address</label
+                                        >
+                                        <input
+                                            type="email"
+                                            class="form-control"
+                                            id="exampleFormControlInput1"
+                                            placeholder="name@example.com"
+                                        />
+                                    </div>
+                                    <div class="mb-3">
+                                        <label
+                                            for="exampleFormControlTextarea1"
+                                            class="form-label"
+                                            >Example textarea</label
+                                        >
+                                        <textarea
+                                            class="form-control"
+                                            id="exampleFormControlTextarea1"
+                                            rows="3"
+                                        ></textarea>
+                                    </div>
+                                    <div class="mb-3">
+                                        <button class="btn btn-primary">
+                                            Send
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <!-- End Content -->
     </body>
 </html>
